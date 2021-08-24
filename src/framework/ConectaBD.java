b4201399ff2c1a1f62b1f565a5106bc0f7bfd297
willjsantos <wjs@discente.ifpe.edu.br>
2021-08-24 15:55:41 -0300

    VERSAO FUNCIONAL - consulta ao banco ok

--- src/framework/ConectaBD.java
+++ src/framework/ConectaBD.java
@@ -1,16 +1,54 @@
 package framework;
 
+import java.net.http.HttpClient.Version;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConectaBD {
 	
-	public static final Connection con = null ;
-	public static final Statement stmt = null;
-	public static final ResultSet rs = null ;
-	//public static final User user = null;
-	public static final String url = "jdbc:mysql://localhost:3306";
-	
+    private Connection con = null;
+    private Statement st = null;
+    private ResultSet rs = null;
+    private String url = "jdbc:mysql://localhost:3306/aula";
+    private String user = "root";
+    private String password = "";
+    
+    public void conecta () {
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("select * from pessoas;");
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
 
 }
--- src/util/App.java
+++ src/util/App.java
@@ -1,36 +1,13 @@
 package util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import framework.ConectaBD;
 
-import framework.CriateDataBase;
-import framework.DriveManager;
+public class App {
 
-public class App implements DriveManager {
+public static void main(String[] args) {
+
+	ConectaBD con = new ConectaBD();
 	
-	public static void main(String[] args) throws SQLException  {
-		
-		Connection con = null ;
-		Statement stmt = null;
-		ResultSet rs = null ;
-		//User user = null;
-		String url = "jdbc:mysql://localhost:3306";
-		
-		CriateDataBase teste = new CriateDataBase();
-		
-		teste.teste();
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriveManager.getConnection(url, "root","");
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-
+	con.conecta();
+ }
 }
